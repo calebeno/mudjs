7e4b5fd66a4904498c899a55d974595a092afa99
Caleb Eno <caleb.eno@gmail.com>
2018-12-22 19:27:25 -0500

    Getting Tests to run with Jest

--- src/lib/number.spec.ts
+++ src/lib/number.spec.ts
@@ -2,9 +2,10 @@
 // import test from 'ava';
 // import { double, power } from './number';
 //
-// test('double', t => {
-//   t.is(double(2), 4);
-// });
+test('double', () => {
+  // t.is(double(2), 4);
+    expect(true).toBe(true);
+});
 //
 // test('power', t => {
 //   t.is(power(2, 4), 16);
--- src/lib/room/room.spec.ts
+++ src/lib/room/room.spec.ts
@@ -1,9 +1,12 @@
+// @ts-ignore
+import uuid = require('uuid');
 import { createRoom } from './room';
 
 test('first', () => {
-    // const config: MUDRoomConfig= {
-    //     dummyParam: ''
-    // };
+    const mockUUID = 'abc-123';
+    const v4Spy = jest.spyOn(uuid, 'v4').mockReturnValue(mockUUID);
+
     const room = createRoom();
-    expect(room.roomID).toBe('');
+    expect(room.roomID).toBe(mockUUID);
+    expect(v4Spy).toHaveBeenCalledTimes(1);
 });
