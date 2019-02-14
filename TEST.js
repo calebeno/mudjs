9221870e292c78b80f2449747ef46a748135ac07
Caleb Eno <caleb.eno@gmail.com>
2019-02-13 22:28:39 -0500

    Fixing Room Spec

--- TEST.js
+++ TEST.js
@@ -4,9 +4,9 @@ let test = require('./build/main/index.js');
 
 let mud = test.initializeNewMUDjs({});
 console.log(mud.config());
-console.log(mud.createLevel(2, 2));
+console.log(mud.createLevel(3, 3));
 console.log(mud.level(0).info());
 console.log(mud.level(0).rooms());
 console.log('---------');
-console.log(mud.level(0).roomByCoordinates(0, 0).info());
+console.log(mud.level(0).roomByCoordinates(0, 2).info());
 
--- src/lib/room/room.spec.ts
+++ src/lib/room/room.spec.ts
@@ -1,11 +1,12 @@
 // @ts-ignore
 import uuid = require('uuid');
+import { createShellRoom } from './room';
 
-// test('first', () => {
-//     const mockUUID = 'abc-123';
-//     const v4Spy = jest.spyOn(uuid, 'v4').mockReturnValue(mockUUID);
-//
-//     const room = createRoom();
-//     expect(room.roomID).toBe(mockUUID);
-//     expect(v4Spy).toHaveBeenCalledTimes(1);
-// });
+test('first', () => {
+    const mockUUID: string = 'abc-123';
+    const v4Spy = jest.spyOn(uuid, 'v4').mockReturnValue(<any>mockUUID);
+
+    const room = createShellRoom(0, 0);
+    expect(room.info().roomID).toBe(mockUUID);
+    expect(v4Spy).toHaveBeenCalledTimes(1);
+});
