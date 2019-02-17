361e3fa9421f77a9f8433ff548c4abbc0f4f39e5
Caleb Eno <caleb.eno@gmail.com>
2019-02-16 21:27:30 -0500

    fix: weirdness

--- src/game/game.ts
+++ src/game/game.ts
@@ -16,29 +16,29 @@ export function MUDBuildGame(config: MUDGameConfig): MUDGame {
         return levels
     }
 
-    function getLevel(id: number | string): MUDLevel | null {
-        if (typeof id === 'number') {
-            if (levels[id]) {
-                return levels[id]
-            } else {
-                handleError(`Room not found with index ${id}`)
-                return null
-            }
-        }
-
-        if (typeof id === 'string') {
-            const levelMatch = levels.find(level => level.info().levelID === id)
-            if (levelMatch) {
-                return levelMatch
-            } else {
-                // tslint:disable-next-line
-                handleError(`Room not found with identifier ${id}`)
-                return null
-            }
-        }
-        handleError('No valid room identifier provided')
-        return null
-    }
+    // function getLevel(id: number | string): MUDLevel | null {
+    //     if (typeof id === 'number') {
+    //         if (levels[id]) {
+    //             return levels[id]
+    //         } else {
+    //             handleError(`Room not found with index ${id}`)
+    //             return null
+    //         }
+    //     }
+    //
+    //     if (typeof id === 'string') {
+    //         const levelMatch = levels.find(level => level.info().levelID === id)
+    //         if (levelMatch) {
+    //             return levelMatch
+    //         } else {
+    //             // tslint:disable-next-line
+    //             handleError(`Room not found with identifier ${id}`)
+    //             return null
+    //         }
+    //     }
+    //     handleError('No valid room identifier provided')
+    //     return null
+    // }
 
     function createLevelWrapper(x: number, y: number): MUDLevel {
         return MUDcreateLevel(config, levels, x, y)
@@ -47,7 +47,7 @@ export function MUDBuildGame(config: MUDGameConfig): MUDGame {
     return {
         config: getConfig,
         levels: getLevels,
-        level: getLevel,
+        // level: getLevel,
         createLevel: createLevelWrapper
     }
 }
@@ -60,6 +60,6 @@ export interface MUDGameConfig {
 export interface MUDGame {
     readonly config: () => MUDGameConfig
     readonly levels: () => MUDLevel[]
-    readonly level: (id: number | string) => MUDLevel | null
+    // readonly level: (id: number | string) => MUDLevel | null
     readonly createLevel: (x: number, y: number) => MUDLevel
 }
