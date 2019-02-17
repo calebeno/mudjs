815e66fe90b2b3eb85ef9a63ec86164d2ba76b80
Caleb Eno <caleb.eno@gmail.com>
2019-02-17 16:26:36 -0500

    fix: version and build issues

--- rollup.config.ts
+++ rollup.config.ts
@@ -4,35 +4,37 @@ import sourceMaps from 'rollup-plugin-sourcemaps'
 import camelCase from 'lodash.camelcase'
 import typescript from 'rollup-plugin-typescript2'
 import json from 'rollup-plugin-json'
+import builtins from 'rollup-plugin-node-builtins';
 
 const pkg = require('./package.json')
 
 const libraryName = 'mudjs'
 
 export default {
-  input: `src/${libraryName}.ts`,
-  output: [
-    { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
-    { file: pkg.module, format: 'es', sourcemap: true },
-  ],
-  // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: [],
-  watch: {
-    include: 'src/**',
-  },
-  plugins: [
-    // Allow json resolution
-    json(),
-    // Compile TypeScript files
-    typescript({ useTsconfigDeclarationDir: true }),
-    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
-    commonjs(),
-    // Allow node_modules resolution, so you can use 'external' to control
-    // which external modules to include in the bundle
-    // https://github.com/rollup/rollup-plugin-node-resolve#usage
-    resolve(),
+    input: `src/${libraryName}.ts`,
+    output: [
+        { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true, globals: {crypto: 'crypto'} },
+        { file: pkg.module, format: 'es', sourcemap: true, globals: {crypto: 'crypto'}}
+    ],
+    // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
+    external: [],
+    watch: {
+        include: 'src/**'
+    },
+    plugins: [
+        builtins(),
+        // Allow json resolution
+        json(),
+        // Compile TypeScript files
+        typescript({ useTsconfigDeclarationDir: true }),
+        // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
+        commonjs(),
+        // Allow node_modules resolution, so you can use 'external' to control
+        // which external modules to include in the bundle
+        // https://github.com/rollup/rollup-plugin-node-resolve#usage
+        resolve(),
 
-    // Resolve source maps to the original source
-    sourceMaps(),
-  ],
+        // Resolve source maps to the original source
+        sourceMaps()
+    ]
 }
